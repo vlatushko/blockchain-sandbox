bd31fb779faf98984f15a8b866f8193301ed5f54
Vasyl Latushko <vasyl.latushko@gmail.com>
2023-04-03 13:00:32 +0300

    Initial commit

---
+++ Block.h
@@ -0,0 +1,11 @@
+//
+// Created by vasyl on 4/3/23.
+//
+
+#ifndef BLOCKCHAIN_SANDBOX_BLOCK_H
+#define BLOCKCHAIN_SANDBOX_BLOCK_H
+
+#include <cstdint>
+#include <iostream>
+
+#endif //BLOCKCHAIN_SANDBOX_BLOCK_H
